cd332f962872a1f5dc689dc99a95c98dcdf890d6
juelhossain <juelhossainnn@gmail.com>
2022-08-05 20:47:05 +0600

    trying again

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,13 +1,12 @@
 const createHttpError = require("http-errors");
 
 const notFoundHandler = (req, res, next) => {
-    next(createHttpError(404, "your requested content was not found"));
-}
+  next(createHttpError(404, "your requested content was not found"));
+};
 
 const errorHandler = (err, req, res, next) => {
-    console.log('this is the error',err);
-    res.send(err);
-    next();
-}
+  res.send(err);
+  next(err);
+};
 
 module.exports = { notFoundHandler, errorHandler };
